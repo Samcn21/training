dd7d3a7e14a8298af11a10d2cd779eb19a43b174
Setareh Malekdoust <malekdoust.setareh@gmail.com>
2022-04-14 17:18:30 +0430

    220414:loops are rewrited

--- python/family-relationship/family-tree-first-script.py
+++ python/family-relationship/family-tree-first-script.py
@@ -4,59 +4,40 @@ import collections
 with open('../../data/family-tree.json') as jsonFile:
     familyTreeData = json.load(jsonFile)
 
+bothGenders = {}
+males = {}
+females = {}
 
-# this function gets all values of the key in the data file
-def getValues(data, key):
-    values = []
-    for i in range(len(data)):
-        if i not in values:
-            values.append(data[i][key])
-    return values
+for i in range(len(familyTreeData)):
+    personObject = familyTreeData[i]
+    name = personObject['name']
+    roles = personObject['role']
+    for j in range(len(roles)):
+        role = roles[j]
+        if role == 'Father' or role == "Son" or role == 'Brother' or role == 'Son in law' or role == 'Husband' :  
+            males.update({name:': Male'}) , bothGenders.update({name:': Male'})
+            break
+        else :
+            females.update({name:': Female'}) , bothGenders.update({name:': Female'})
+            break
 
-
-names = getValues(familyTreeData, "name")
-roles = getValues(familyTreeData, "role")
-
-
-kind = ""
-for i in range(len(roles)):
-    if "Son" in roles[i]:  
-        kind += "Male "
-    elif "Father" in roles[i]:
-        kind += "Male " 
-    else :
-        kind += "Female "
-gender = kind.split()
-
-
-nameGenderDictionary= {names[i]+":" : gender[i] for i in range(len(names))}
-sortedDictionary = collections.OrderedDict(sorted(nameGenderDictionary.items()))
-
-
-def getSpecificKeys(mainGenderDictionary, value):
-    dividedGenderDictionary = {}
-    for i in mainGenderDictionary:
-        if mainGenderDictionary[i] == value:
-            dividedGenderDictionary[i] = mainGenderDictionary[i]
-    return dividedGenderDictionary 
-
-malesDictionary=getSpecificKeys(sortedDictionary,"Male")
-femalesDictionary=getSpecificKeys(sortedDictionary,"Female")
-
+sortedBothGenders = collections.OrderedDict(sorted(bothGenders.items()))
+sortedMales = collections.OrderedDict(sorted(males.items()))
+sortedFemales = collections.OrderedDict(sorted(females.items()))
 
 isLoop = True
 while isLoop :
     mainListQuestion = input("Do you want to see the list of all the people? yes / no? ").lower()
     if mainListQuestion == "yes" or mainListQuestion == "y":
-        for key, value in sortedDictionary.items(): print (key, value)  
+        for key, value in sortedBothGenders.items(): print (key, value)  
         isLoop = False
     elif mainListQuestion == "no" or mainListQuestion == "n": 
         genderListQuestion=input ("Do you want to see list of males or females? ").lower()
         if genderListQuestion=="males" or genderListQuestion=="m":
-            for key, value in malesDictionary.items(): print (key, value)
+            for key, value in sortedMales.items(): print (key, value)
             isLoop = False
         elif genderListQuestion == "females" or genderListQuestion=="f":
-            for key, value in femalesDictionary.items(): print (key, value)
+            for key, value in sortedFemales.items(): print (key, value)
             isLoop = False
         else:
             print("Please type males or females")
--- python/family-relationship/family-tree-second-script.py
+++ python/family-relationship/family-tree-second-script.py
@@ -1,49 +1,42 @@
-
 import json
-
 with open('../../data/family-tree.json') as jsonFile:
     familyTreeData = json.load(jsonFile)
 
-#change the names
-agesList=[]
-for i in range(len(familyTreeData)):
-    if i not in agesList:   
-        agesList.append(familyTreeData[i]["age"])
-       
-        
 ageGroups =["0-10", "11-20", "21-30", "31-40", "41-50", "51-60", "61-70","71-80","81-90","91-100", "+100"]
-totalList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+ageGroupsCounter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for i in range(len(familyTreeData)):
-    if agesList[i] <=10:
-        totalList[0] +=1
-    elif agesList[i] <=20:
-        totalList[1] +=1
-    elif agesList[i] <=30:
-        totalList[2] +=1
-    elif agesList[i] <=40:
-        totalList[3] +=1
-    elif agesList[i] <=50:
-        totalList[4] +=1
-    elif agesList[i] <=60:
-        totalList[5] +=1
-    elif agesList[i] <=70:
-        totalList[6] +=1
-    elif agesList[i] <=80:
-        totalList[7] +=1
-    elif agesList[i] <=90:
-        totalList[8] +=1
-    elif agesList[i] <=100:
-        totalList[9] +=1
+    personObject = familyTreeData[i]
+    age = personObject['age']
+   
+    if age <= 10:
+        ageGroupsCounter[0] +=1
+    elif age <= 20:
+        ageGroupsCounter[1] +=1
+    elif age <= 30:
+        ageGroupsCounter[2] +=1
+    elif age <= 40:
+        ageGroupsCounter[3] +=1
+    elif age <= 50:
+        ageGroupsCounter[4] +=1
+    elif age <= 60:
+        ageGroupsCounter[5] +=1
+    elif age <= 70:
+        ageGroupsCounter[6] +=1
+    elif age <= 80:
+        ageGroupsCounter[7] +=1
+    elif age <= 90:
+        ageGroupsCounter[8] +=1
+    elif age <= 100:
+        ageGroupsCounter[9] +=1
     else:
-        totalList[10] +=1
+        ageGroupsCounter[10] +=1
             
 personOrPersons=[]
-for i in totalList:
-    if totalList[i] <=1:
+for i in range(len(ageGroupsCounter)):
+    if ageGroupsCounter[i] <=1:
         personOrPersons.append("person")
     else:
         personOrPersons.append("persons")
-
-for i in range(len(totalList)):   
-    print(str(ageGroups[i])+"\n" +str(totalList[i])+" "+str(personOrPersons[i])+"\n")
+   
+    print(str(ageGroups[i])+"\n" +str(ageGroupsCounter[i])+" "+str(personOrPersons[i])+"\n")
