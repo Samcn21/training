48376bd7127365b217f20fd5af7ecf00c73652a9
Setareh Malekdoust <malekdoust.setareh@gmail.com>
2022-04-17 19:47:34 +0430

    20220417-nested loops are written for age groups project

--- python/family-relationship/family-tree-second-script.py
+++ python/family-relationship/family-tree-second-script.py
@@ -2,13 +2,13 @@ import json
 with open('../../data/family-tree.json') as jsonFile:
     familyTreeData = json.load(jsonFile)
 minAge = 0
-maxAge = 70
+maxAge = 100
 interval = 10
 stats=[]
 for i in range(minAge,maxAge,interval):
     min = i
     max = i + interval -1
-    text = "Age Group: " + str(min) + "-" + str(max) + ": "
+    text = "Age Group: " + str(min) + "-" + str(max) + ": \n"
         
     total = 0
     for j in range(len(familyTreeData)):
@@ -16,6 +16,9 @@ for i in range(minAge,maxAge,interval):
         age = person['age']
         if age >= min and age <= max:
             total += 1
+    if total <= 1:
+        total =str(total) + str(" person")
+    else:
+        total =str(total) + str(" persons")
     stats=(str(text)+str(total))
-    
     print(stats)
