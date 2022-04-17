81ea9ab6f2b0c0bdb0ecaa80419202aab85e3ccf
Setareh Malekdoust <malekdoust.setareh@gmail.com>
2022-04-17 18:26:07 +0430

    20220417-nested loops are written for age groups project

--- python/family-relationship/family-tree-second-script.py
+++ python/family-relationship/family-tree-second-script.py
@@ -1,42 +1,21 @@
 import json
 with open('../../data/family-tree.json') as jsonFile:
     familyTreeData = json.load(jsonFile)
-
-ageGroups =["0-10", "11-20", "21-30", "31-40", "41-50", "51-60", "61-70","71-80","81-90","91-100", "+100"]
-ageGroupsCounter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-for i in range(len(familyTreeData)):
-    personObject = familyTreeData[i]
-    age = personObject['age']
-   
-    if age <= 10:
-        ageGroupsCounter[0] +=1
-    elif age <= 20:
-        ageGroupsCounter[1] +=1
-    elif age <= 30:
-        ageGroupsCounter[2] +=1
-    elif age <= 40:
-        ageGroupsCounter[3] +=1
-    elif age <= 50:
-        ageGroupsCounter[4] +=1
-    elif age <= 60:
-        ageGroupsCounter[5] +=1
-    elif age <= 70:
-        ageGroupsCounter[6] +=1
-    elif age <= 80:
-        ageGroupsCounter[7] +=1
-    elif age <= 90:
-        ageGroupsCounter[8] +=1
-    elif age <= 100:
-        ageGroupsCounter[9] +=1
-    else:
-        ageGroupsCounter[10] +=1
-            
-personOrPersons=[]
-for i in range(len(ageGroupsCounter)):
-    if ageGroupsCounter[i] <=1:
-        personOrPersons.append("person")
-    else:
-        personOrPersons.append("persons")
-   
-    print(str(ageGroups[i])+"\n" +str(ageGroupsCounter[i])+" "+str(personOrPersons[i])+"\n")
+minAge = 0
+maxAge = 70
+interval = 10
+stats=[]
+for i in range(minAge,maxAge,interval):
+    min = i
+    max = i + interval -1
+    text = "Age Group: " + str(min) + "-" + str(max) + ": "
+        
+    total = 0
+    for j in range(len(familyTreeData)):
+        person = familyTreeData[j]
+        age = person['age']
+        if age >= min and age <= max:
+            total += 1
+    stats=(str(text)+str(total))
+    
+    print(stats)
