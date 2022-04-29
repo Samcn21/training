4f7984558faf76d4252fcdb346e7079e94eabc4e
Moeen Heydari <moeentk@gmail.com>
2022-04-29 15:34:06 +0430

    220429:css-javascript-ageGroups-new-style

---
+++ web-development/family-tree/age-groups/documents/data.js
@@ -0,0 +1,155 @@
+var data = 
+`[
+    { 
+        "name": "Setareh",
+        "age": 33,
+        "children": 0,
+        "married": true,
+        "role": [
+            "Wife",
+            "Daughter",
+            "Sister",
+            "Sister in law",
+            "Daughter in Law"
+        ],
+        "something": {
+            "test": true,
+            "showcase": {
+                "slides": [
+                    {
+                        "name": "testo"
+                    },
+                    {
+                        "name": "festo"
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "name": "Moeen",
+        "age": 32,
+        "children": 0,
+        "married": true,
+        "role": [
+            "Husband",
+            "Son",
+            "Brother",
+            "Brother in law",
+            "Son in Law"
+        ]
+    },
+    {
+        "name": "Sayid",
+        "age": 38,
+        "children": 0,
+        "married": false,
+        "role": [
+            "Son",
+            "Brother",
+            "Brother in Law"
+        ]
+    },
+    {
+        "name": "Mohsen",
+        "age": 35,
+        "children": 1,
+        "married": true,
+        "role": [
+            "Husband",
+            "Son",
+            "Father",
+            "Brother",
+            "Brother in law"
+        ]
+    },
+    {
+        "age": 35,
+        "children": 1,
+        "married": true,
+        "name": "Behnaz",
+        "role": [
+            "Wife",
+            "Sister in law",
+            "Mother",
+            "Daughter in Law"
+        ]
+    },
+    {
+        "age": 3,
+        "children": 0,
+        "married": false,
+        "name": "Delara",
+        "role": [
+            "Daughter",
+            "Grand Daughter"
+        ]
+    },
+    {
+        "age": 30,
+        "children": 0,
+        "married": true,
+        "name": "Hussein",
+        "role": [
+            "Husband",
+            "Son",
+            "Brother",
+            "Brother in law"
+        ]
+    },
+    {
+        "age": 29,
+        "children": 0,
+        "married": true,
+        "name": "Zahra",
+        "role": [
+            "Wife",
+            "Sister in law",
+            "Daughter in Law"
+        ]
+    },
+    {
+        "age": 59,
+        "children": 3,
+        "married": true,
+        "name": "Esfandyar",
+        "role": [
+            "Husband",
+            "Father",
+            "Grand Father",
+            "Father in law"
+        ]
+    },
+    {
+        "age": 54,
+        "children": 3,
+        "married": true,
+        "name": "Katayoon",
+        "role": [
+            "Wife",
+            "Mother",
+            "Grand Mother",
+            "Mother in law"
+        ]
+    },
+    {
+        "age": 66,
+        "children": 2,
+        "married": true,
+        "name": "Rahim",
+        "role": [
+            "Father",
+            "Father in law"
+        ]
+    },
+    {
+        "age": 66,
+        "children": 2,
+        "married": true,
+        "name": "Robabeh",
+        "role": [
+            "Mother",
+            "Mother in law"
+        ]
+    }
+]`;
