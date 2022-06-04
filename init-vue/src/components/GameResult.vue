55d1c8dc29ae03c4e963a3c909ea8c073d683dc6
Moeen Heydari <moeentk@gmail.com>
2022-06-04 17:42:02 +0430

    220604: tic-tac-toe started

---
+++ init-vue/src/components/GameResult.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+</template>
+
+<script>
+export default {
+  props: ['result'],
+  methods: {
+    sendResult() {
+    this.$emit('end', this.result)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
